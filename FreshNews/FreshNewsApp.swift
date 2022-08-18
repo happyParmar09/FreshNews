77947f396e13954f3ac2c543683cd1d59ee21172
Gokul Kumar Parmar <parmarhappy191@gmail.com>
2022-08-19 01:07:43 +0530

    Initial Commit

---
+++ FreshNews/FreshNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshNewsApp.swift
+//  FreshNews
+//
+//  Created by gokulparmar on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FreshNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

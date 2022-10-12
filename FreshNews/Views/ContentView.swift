f8c953be7d4de2e1c6be90fa3bcc44da08fbedcd
Gokul Kumar Parmar <parmarhappy191@gmail.com>
2022-10-12 16:41:49 +0530

    New Changes

---
+++ FreshNews/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  FreshNews
+//
+//  Created by gokulparmar on 19/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts){ post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationBarTitle("FreshNews")
+        }
+        .onAppear{
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreshNews/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  FreshNews
+//
+//  Created by gokulparmar on 19/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct DetailView : View{
+    let url : String?
+    
+    var body : some View{
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews : PreviewProvider{
+    static var previews: some View {
+        DetailView(url : "https://www.google.com")
+    }
+}
+
